2a8121a00632699a126cbc9dbf70dc4b8ce2bf49
Navil260 <hlod260@gmail.com>
2025-04-01 16:20:36 +0700

    firebaseConfig

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:project/screen/goal_tracker.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+          apiKey: "AIzaSyDHxt6mOyBexwBluBBnWY-OEWiWcMcFkt8",
+          authDomain: "gold-gold-3a89f.firebaseapp.com",
+          projectId: "gold-gold-3a89f",
+          storageBucket: "gold-gold-3a89f.firebasestorage.app",
+          messagingSenderId: "64206464929",
+          appId: "1:64206464929:web:0023767a9f197a27b1cde0",
+          measurementId: "G-NZRL07K7B6"));
   runApp(const MyApp());
 }
 
@@ -16,9 +27,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
-      home: const OnboardingScreen(), // เปลี่ยนจาก HomeScreen เป็น OnboardingScreen
+      home:
+          const OnboardingScreen(), // เปลี่ยนจาก HomeScreen เป็น OnboardingScreen
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
